d4e5258cbe672e01a7cd4baf5022325ed87db142
Ebenézer <ebenezerdorneles@gmail.com>
2025-11-14 12:21:51 +0000

    feat: adicionar visualização de dados de municípios e escolas em Santa Maria

--- scripts/aula-4-geobr.R
+++ scripts/aula-4-geobr.R
@@ -2,7 +2,35 @@
 
 install.packages("geobr")
 
-
 # DATA --------------------------------------------------------------------
 
+municipios <- geobr::read_municipality(code_muni = "RS")
+escolas <- geobr::read_schools(year = 2023)
 
+dplyr::glimpse(municipios)
+dplyr::glimpse(escolas)
+
+# STATE = RS --------------------------------------------------------------
+
+ggplot2::ggplot(data = municipios) +
+  ggplot2::geom_sf()
+
+# MUNI = SANTA MARIA ------------------------------------------------------
+
+municipios |>
+  dplyr::filter(name_muni == "Santa Maria" | code_muni == 4316907) |>
+ggplot2::ggplot() +
+  ggplot2::geom_sf()
+
+# MUNI = SANTA MARIA & SCHOOL ---------------------------------------------
+
+df_school <- escolas |>
+  dplyr::filter(name_muni == "Santa Maria" & abbrev_state == "RS")
+
+df_santa_maria <- municipios |>
+  dplyr::filter(name_muni == "Santa Maria" | code_muni == 4316907)
+
+ggplot2::ggplot(data = df_school) +
+  ggplot2::geom_sf(data = df_santa_maria) +
+  ggplot2::geom_sf(ggplot2::aes(color = urban)) +
+  ggplot2::facet_wrap(~ government_level)
